4ba907b620729c3e013c5bbd98da1835bcc8f938
k3yart <iceopera@gmail.com>
2018-05-22 16:49:26 +0300

    first commit

---
+++ Source/SuperShooter/SuperShooterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SuperShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASuperShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASuperShooterGameMode();
+};
+
+
+
